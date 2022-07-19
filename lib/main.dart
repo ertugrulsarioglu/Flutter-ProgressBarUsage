ca0ca4e1cc82986e1838f265d03f5a0852e7f075
ertugrul <sariertugrull@gmail.com>
2022-07-19 13:25:36 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  bool progressBarGorun = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Visibility(
+              visible: progressBarGorun,
+              child: CircularProgressIndicator(
+                valueColor: AlwaysStoppedAnimation<Color>(Colors.deepPurple),
+              ),
+            ),
+            ElevatedButton(
+                child: Text("Başla"),
+                onPressed: (){
+                  setState(() {
+                    progressBarGorun = true;
+                  });
+                },
+            ),
+            ElevatedButton(
+              child: Text("Dur"),
+              onPressed: (){
+                setState(() {
+                  progressBarGorun = false;
+                });
+              },
+            ),
+
+          ],
+        ),
+      ),
+
+    );
+  }
+}
